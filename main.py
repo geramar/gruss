ad0d744387047ff53653373a2cc9b9321624897d
geramar <gerashemar@gmail.com>
2020-03-03 22:42:52 +0300

    работает на формулах без перемещений и только там где профессор еще надо переназначить адд

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from classFormula import Formula
 
-fileName = str(input('Название файла: ')+ '.txt')
-inFile = open(fileName, 'r', encoding = 'utf8').read()
-data = Formula(inFile)
-print(data.getFormula())
+for i in range(1, 5):
+    fileName = str(i) + '.txt'
+    #fileName = str(input('Название файла: ')+ '.txt')
+    inFile = open(fileName, 'r', encoding = 'utf8').read()
+    data = Formula(inFile)
+    print(i, data.getFormula())
