2a5d83364ed3041cafea3f185efa2d244bcfe233
geramar <gerashemar@gmail.com>
2020-03-12 02:47:34 +0300

    валидатор починился

--- classValidator.py
+++ classValidator.py
@@ -7,35 +7,36 @@ class Validator:
         self.formula = formula
 
     def validate(self):
-        for word in self.formula:
-            if word == 'h':
-                value = False
-                beginning = self.formula.index('h')
-                i_end = len(self.formula)
+        value = False
+        beginning = 0
+        for type in self.formula:
+            if type == 'h1':
+                beginning = self.formula.index('h1')
+                break
 
-                for type in self.formula[::-1]:
-                    i_end -= 1
-                    if not isinstance(type, UnknownType):
-                        break
-                ending = i_end
+        i_end = len(self.formula)
+        for type in self.formula[::-1]:
+            i_end -= 1
+            if not isinstance(type, UnknownType):
+                 break
+        ending = i_end
 
-                i = 0
-                test = 0
-                for type in self.formula:
-                    for el in range(i, len(self.ordnung)):
-                        if type == 'i' or type == self.ordnung[el]:
-                            i = el + 1
-                            test += 1
-                            break
-
-                if test == len(self.formula[beginning:ending + 1]):
-                    if beginning == 0 and ending == len(self.formula) - 1:
-                        value = 'Totally correct'
-                    elif beginning == 0:
-                        value = 'Correct until ' + str(ending + 1) + 'th word'
-                    elif ending == self.formula[-1]:
-                        value = 'Correct starting from ' + str(beginning + 1)
-                    else:
-                        value = 'Correct starting from ' + str(beginning + 1) + 'th word ' + 'ending at ' + str(
-                            ending + 1) + 'th word'
-                return value
+        i = 0
+        test = 0
+        for type in self.formula:
+            for el in range(i, len(self.ordnung)):
+                if type == 'i' or type == self.ordnung[el]:
+                    i = el + 1
+                    test += 1
+                    break
+        if test == len(self.formula[beginning:ending]):
+            if beginning == 0 and ending == len(self.formula) - 1:
+                value = 'Totally correct'
+            elif beginning == 0:
+                value = 'Correct until ' + str(ending + 1) + 'th word'
+            elif ending == self.formula[-1]:
+                value = 'Correct starting from ' + str(beginning + 1)
+            else:
+                value = 'Correct starting from ' + str(beginning + 1) + 'th word ' + 'ending at ' + str(
+                    ending + 1) + 'th word'
+        return value
